c20a68bc9f53370a31bd8a4f3e6c9afeeed2280e
GenericallyNamed <statefan0826@gmail.com>
2022-12-23 15:52:53 -0500

    added more probably brokey

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,31 @@
+// based on multi-threaded web server tutorial at https://doc.rust-lang.org/book/ch20-01-single-threaded.html
+
 use std::net::TcpListener;
+use std::{
+    io::{prelude::*, BufReader},
+    net::{TcpListener, TcpStream},
+};
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:9000").unwrap();
 
     for stream in listener.incoming() {
         let _stream = stream.unwrap();
-
+        handle_connection(_stream);
         println!("Connection established!");
     }
+}
+
+fn handle_connection(mut stream: TcpStream) {
+    let buf_reader = BufReader::new(&mut stream);
+    let http_request: Vec<_> = buf_reader
+        .lines()
+        .map(|result| result.unwrap())
+        .take_while(|line| !line.is_empty())
+        .collect();
+    println!("Request: {:#?}", http_request);
+
+    let response = "HTTP/1.1 200 OK\r\n\r\n";
+
+    stream.write_all(response.as_bytes()).unwrap();
 }
